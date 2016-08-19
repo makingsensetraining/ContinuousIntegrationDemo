033b3a9e165d7147f961495bb263d64da3e0750e
Andres Moschini <andresmoschini@gmail.com>
2016-08-19 16:23:35 -0300

    Initial commit with a simple project

---
+++ Net SDK Unit Tests/ServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetSDK;
+
+namespace Net_SDK_Unit_Tests
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        [TestMethod]
+        public void Demo_result_should_starts_with_hi()
+        {
+            //Arrange
+            var sut = new Service();
+
+            //Act
+            var result = sut.Demo("name");
+
+            //Assert
+            Assert.IsTrue(result.StartsWith("Hi"));
+        }
+
+        [TestMethod]
+        public void Demo_result_should_ends_with_the_name()
+        {
+            //Arrange
+            var sut = new Service();
+            var name = "name";
+
+            //Act
+            var result = sut.Demo(name);
+
+            //Assert
+            Assert.IsTrue(result.EndsWith(name));
+        }
+    }
+}
---
+++ NetSDK/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetSDK
+{
+    public class Service
+    {
+        public string Demo(string username)
+        {
+            return "Hi " + username;
+        }
+    }
+}
